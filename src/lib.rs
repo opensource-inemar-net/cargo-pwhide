00cf3a8e3604b51fc4677cb3e09401db3ba6300f
Oliver Schnabl <oliver.schnabl@drei.com>
2023-08-24 16:51:58 +0200

    split in lib and main

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,83 @@
 use anyhow::anyhow;
+use chacha20poly1305::aead::generic_array::GenericArray;
+use chacha20poly1305::aead::Aead;
+use chacha20poly1305::aead::OsRng;
+use chacha20poly1305::AeadCore;
 use chacha20poly1305::ChaCha20Poly1305;
 use chacha20poly1305::KeyInit;
-use chacha20poly1305::aead::Aead;
-use chacha20poly1305::aead::generic_array::GenericArray;
-use sha2::{Sha256, Digest};
+use sha2::{Digest, Sha256};
 
-pub fn decrypt(packagename:impl Into<String>,data:impl Into<String>) -> anyhow::Result<String> {
-    let secret:String=packagename.into();
+#[macro_export]
+macro_rules! pwdecrypt {
+    ($value:expr) => {
+        cargo_pwhide::pwdecrypt(env!("CARGO_PACKAGE_NAME"), $value)
+    };
+}
+
+pub fn pwdecrypt(
+    packagename: impl Into<String>,
+    data: impl Into<String>,
+) -> anyhow::Result<String> {
+    let secret: String = packagename.into();
     let mut hasher = Sha256::new();
     hasher.update(secret);
 
-
     let cipher = ChaCha20Poly1305::new(&hasher.finalize());
 
-    let value:String=data.into();
-    if value.len()<18 {
-        return Err(anyhow!("Decoding not possible, encrypted value is too short"))
+    let value: String = data.into();
+    if value.len() < 18 {
+        return Err(anyhow!(
+            "Decoding not possible, encrypted value is too short"
+        ));
     }
-    let (nonce,text)=value.split_at(17);
-    let nonce=if let Ok(dec)=base_62::decode(nonce) {
+    let (nonce, text) = value.split_at(17);
+    let nonce = if let Ok(dec) = base_62::decode(nonce) {
         GenericArray::clone_from_slice(dec.as_slice())
     } else {
         return Err(anyhow!("Decoding not possible, encrypted value is invalid"));
     };
-    
-    let text=if let Ok(dec)=base_62::decode(text) {
+
+    let text = if let Ok(dec) = base_62::decode(text) {
         dec
     } else {
-        return Err(anyhow!("Decoding not possible, encrypted value is invalid"))
-        
+        return Err(anyhow!("Decoding not possible, encrypted value is invalid"));
     };
-    let plain=match cipher.decrypt(&nonce,text.as_ref()) {
+    let plain = match cipher.decrypt(&nonce, text.as_ref()) {
         Ok(dec) => dec,
-        Err(e) => { 
-            return Err(anyhow!("Decryption failed with {:?}",e));
-            
-        },
+        Err(e) => {
+            return Err(anyhow!("Decryption failed with {:?}", e));
+        }
     };
-    if let Ok(dec)=String::from_utf8(plain) {
+    if let Ok(dec) = String::from_utf8(plain) {
         Ok(dec)
     } else {
         Err(anyhow!("Decryption failed decypted data is no vailid text"))
+    }
+}
+
+pub fn pwencrypt(
+    packagename: impl Into<String>,
+    data: impl Into<String>,
+) -> anyhow::Result<String> {
+    let secret: String = packagename.into();
+    let mut hasher = Sha256::new();
+    hasher.update(secret);
+
+    let cipher = ChaCha20Poly1305::new(&hasher.finalize());
+
+    let value: String = data.into();
+
+    let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
+    let ciphertext = cipher.encrypt(&nonce, value.as_bytes().as_ref());
+    match ciphertext {
+        Err(e) => Err(anyhow!(
+            "Encryption failed with error {:?} -raise github issue",
+            e
+        )),
+        Ok(data) => Ok(format!(
+            "{}{}",
+            base_62::encode(nonce.as_slice()),
+            base_62::encode(data.as_slice())
+        )),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-
-
-use chacha20poly1305::AeadCore;
-use chacha20poly1305::ChaCha20Poly1305;
-use chacha20poly1305::KeyInit;
-use chacha20poly1305::aead::OsRng;
-use chacha20poly1305::aead::Aead;
-use chacha20poly1305::aead::generic_array::GenericArray;
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 use std::fs::read_to_string;
 use toml::Table;
 use toml::Value;
-use sha2::{Sha256, Digest};
 
 fn main() {
     let matches =Command::new("pwhide")
@@ -29,129 +20,81 @@ fn main() {
 
     //We know what to do read the Cargo.toml file
 
+    let cargo = read_to_string("./Cargo.toml");
 
-    let cargo=read_to_string("./Cargo.toml");
-    
-    let secret:String =match cargo {
+    let secret: String = match cargo {
         Ok(cargo) => {
-            let toml=cargo.as_str().parse::<Table>();
+            let toml = cargo.as_str().parse::<Table>();
             //println!("toml {:?}",toml);
             match toml {
-                Ok(toml) => {
-                    match toml.get("package") {
-                        Some(package) => {
-                            match package.get("name") {
-                                Some(value) => {
-                                    match value {
-                                        Value::String(s) => {
-                                            s.clone()
-                                        }
-                                        _ => {
-                                            println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
-                                            println!("Failure parsing Cargo.toml [package] name is nit a string");
-                                            return
-                                        }
-                                    }
-                                },
-                                None => {
-                                    println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
-                                    println!("Failure parsing Cargo.toml [package] name is missing");
-                                    return                        
-                                }
+                Ok(toml) => match toml.get("package") {
+                    Some(package) => match package.get("name") {
+                        Some(value) => match value {
+                            Value::String(s) => s.clone(),
+                            _ => {
+                                println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
+                                println!(
+                                    "Failure parsing Cargo.toml [package] name is nit a string"
+                                );
+                                return;
                             }
-
                         },
-                        None =>{
+                        None => {
                             println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
-                            println!("Failure parsing Cargo.toml [package] is missing");
-                            return
+                            println!("Failure parsing Cargo.toml [package] name is missing");
+                            return;
                         }
+                    },
+                    None => {
+                        println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
+                        println!("Failure parsing Cargo.toml [package] is missing");
+                        return;
                     }
-                }
+                },
                 Err(e) => {
-                    println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
-            println!("Failure parsing Cargo.toml cause by {:?}",e);
-            return;
+                    println!(
+                        "The subcommand must be executed in a folder with a valid Cargo.toml file"
+                    );
+                    println!("Failure parsing Cargo.toml cause by {:?}", e);
+                    return;
                 }
             }
-        },
+        }
         Err(e) => {
             println!("The subcommand must be executed in a folder with a valid Cargo.toml file");
-            println!("Failure cause by {:?}",e);
+            println!("Failure cause by {:?}", e);
             return;
         }
     };
 
-    let mut hasher = Sha256::new();
-    hasher.update(secret.as_bytes());
-
-
-    let cipher = ChaCha20Poly1305::new(&hasher.finalize());
-
     //now we decide encrypt or decrypt
-    let value:&String=matches.get_one("password").unwrap();
-    let action:&String=matches.get_one("action").unwrap();
+    let value: &String = matches.get_one("password").unwrap();
+    let action: &String = matches.get_one("action").unwrap();
     match action.as_str() {
-        "encrypt" => {
-            let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
-            let ciphertext = cipher.encrypt(&nonce, value.as_bytes().as_ref());
-            match ciphertext {
-                Err(e) => {
-                    println!("Encryption failed with error {:?}",e);
-                    println!("please raise a github issue");
-                    return
-                },
-                Ok(data) => {
-                    println!("Encrypted: {}{}",base_62::encode(nonce.as_slice()),base_62::encode(data.as_slice()))
-                }
-            };
-            
+        "encrypt" => match cargo_pwhide::pwencrypt(secret, value) {
+            Ok(dec) => {
+                println!("Encrypted: {}", dec);
+            }
+            Err(e) => {
+                println!("Error {:?}", e);
+            }
         },
-        "decrypt" => {
-            if value.len()<18 {
-                println!("Decoding not possible, encrypted value is too short");
-                return;
+        "decrypt" => match cargo_pwhide::pwdecrypt(secret, value) {
+            Ok(dec) => {
+                println!("Decrypted: {}", dec);
             }
-            let (nonce,text)=value.split_at(17);
-            let nonce=if let Ok(dec)=base_62::decode(nonce) {
-                GenericArray::clone_from_slice(dec.as_slice())
-            } else {
-                println!("Decoding not possible, encrypted value is invalid");
-                return;
-            };
-            
-            let text=if let Ok(dec)=base_62::decode(text) {
-                dec
-            } else {
-                println!("Decoding not possible, encrypted value is invalid");
-                return;
-            };
-            let plain=match cipher.decrypt(&nonce,text.as_ref()) {
-                Ok(dec) => dec,
-                Err(e) => { 
-                    println!("Decryption failed with {:?}",e);
-                    return
-                },
-            };
-            if let Ok(dec)=String::from_utf8(plain) {
-                println!("Decrypted: {}",dec)
-            } else {
-                println!("Decryption failed decypted data is no vailid text");
+            Err(e) => {
+                println!("Error {:?}", e);
             }
-            
-            
         },
         _ => {
             println!("Only actions encrypt/decrypt are supported");
-        },
+        }
     }
 
     //let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
     //println!("Nonce: {}",base_62::encode(nonce.as_slice()).len());
     //let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref());
-    
-
-
 
     //println!("Hello, world! {:?}",matches);
 }
